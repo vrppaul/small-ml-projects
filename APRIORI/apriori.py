a655e13941c12fa07540a2c94b0e7e2ec0f84dc1
Pavel Paranin <paraninpasha@gmail.com>
2018-11-21 00:07:35 +0100

    apriori.py is finished

--- APRIORI/apriori.py
+++ APRIORI/apriori.py
@@ -20,4 +20,11 @@ for i in range(len(dataset)):
     transactions.append([str(dataset.values[i, j]) for j in range(len(dataset.iloc[0, :]))])
     
 # Training Apriori on the dataset
-    
+import sys
+sys.path.append('data')
+from apyori import apriori
+rules = apriori(transactions, min_support=3*7/len(dataset), min_confidence=0.2,
+                min_lift=3, min_length=2)
+
+# Visualising the results
+results = list(rules)
